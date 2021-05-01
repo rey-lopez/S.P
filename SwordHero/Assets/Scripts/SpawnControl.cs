bc4b15ddacc8609bfde10b3d7308aef7ed2038ff
rey-lopez <78188472+rey-lopez@users.noreply.github.com>
2021-04-30 20:14:44 -0600

    SH/arch updates

---
+++ SwordHero/Assets/Scripts/SpawnControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnControl : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] enemies;
+    int randomSpawnPoint, randomEnemy;
+    public static bool spawnAllowed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnAllowed = true;
+        InvokeRepeating("SpawnAEnemy", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void SpawnAEnemy()
+    {
+        if (spawnAllowed)
+        {
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            randomEnemy = Random.Range(0, enemies.Length);
+            Instantiate(enemies[randomEnemy], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+        }
+    }
+}
